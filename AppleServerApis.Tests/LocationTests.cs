f6ddd570deda46f9aae3b4b67ff58a98dfd4261a
Paul Irwin <paulirwin@gmail.com>
2023-03-24 14:27:47 -0600

    Add unit tests around Location.DistanceTo, add XML comments

---
+++ AppleServerApis.Tests/LocationTests.cs
@@ -0,0 +1,25 @@
+using AppleServerApis.Maps;
+
+namespace AppleServerApis.Tests;
+
+public class LocationTests
+{
+    [InlineData(0, 0, 0, 0, 0)]
+    [InlineData(0, 0, 0, 1, 111)]
+    [InlineData(0, 0, 1, 0, 111)]
+    [InlineData(0, 0, 1, 1, 157)]
+    [Theory]
+    public void DistanceToTests(double latitudeFrom, 
+        double longitudeFrom, 
+        double latitudeTo, 
+        double longitudeTo,
+        int distanceRoundedKm)
+    {
+        var from = new Location(latitudeFrom, longitudeFrom);
+        var to = new Location(latitudeTo, longitudeTo);
+        
+        var actualDistance = (int)Math.Floor(from.DistanceTo(to) / 1000);
+        
+        Assert.Equal(distanceRoundedKm, actualDistance);
+    }
+}
